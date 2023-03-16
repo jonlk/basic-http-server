e63badd6d1f0bb96ed4f90889e108bd840dc671c
Jon Kovach <jlkovach01@gmail.com>
2023-03-16 19:18:09 -0400

    updates

--- src/process.rs
+++ src/process.rs
@@ -3,7 +3,11 @@ use std::fmt::Error;
 use anyhow::{anyhow, Result};
 
 pub fn process_request(id: u32) -> Result<String> {
-    if id == 12 {
+    let success: bool = true;
+
+    //some processing logic goes here
+
+    if success == true {
         Ok(std::fmt::format(format_args!(
             "processed request for id: {:?}",
             id
