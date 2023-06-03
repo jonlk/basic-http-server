32a40f25ee887d1c307dd740cee2bc94b651b792
Jon Kovach <jlkovach01@gmail.com>
2023-06-02 22:45:07 -0400

    cleaned up responses

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,18 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use process::process_request;
 use regex::Regex;
-use responses::{accepted, bad_request, method_not_allowed, not_found};
 
 use std::convert::Infallible;
+
+use crate::responses::{status, status_with_message};
 
 #[tokio::main]
 pub async fn main() {
     pretty_env_logger::init();
-
     let make_svc = make_service_fn(move |_conn| async { Ok::<_, Infallible>(service_fn(handler)) });
-
     let addr = ([0, 0, 0, 0], 3000).into();
-
     let server = Server::bind(&addr).serve(make_svc);
-
     info!("Listening on http://{}", addr);
-
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
@@ -33,30 +29,30 @@ pub async fn main() {
 fn handle_get(id: u32) -> Response<Body> {
     let pr = process_request(id);
     match pr {
-        Ok(value) => accepted(),
+        Ok(value) => status(StatusCode::ACCEPTED),
         Err(err) => Response::new(Body::from(err.to_string())),
     }
 }
 async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let req_string = req.uri().path_and_query().unwrap().as_str();
     let re = Regex::new(r"^/api/v1/calculate\?id=(\d+$)").unwrap();
-    
+
     info!("Request received");
 
     if !re.is_match(&req_string) {
-        Ok(not_found())
+        Ok(status(StatusCode::NOT_FOUND))
     } else {
         match req.method() {
             &Method::GET => {
                 let capture = re.captures(&req_string).unwrap();
                 let id: u32 = String::from(&capture[1]).parse().unwrap_or_default();
                 if id == 0 {
-                    Ok(not_found())
+                    Ok(status_with_message(StatusCode::NOT_FOUND, "Nope"))
                 } else {
                     Ok(handle_get(id))
                 }
             }
-            _ => Ok(method_not_allowed()),
+            _ => Ok(status(StatusCode::METHOD_NOT_ALLOWED)),
         }
     }
 }
--- src/responses.rs
+++ src/responses.rs
@@ -1,41 +1,5 @@
 use hyper::{Body, Response, StatusCode};
 
-pub fn not_found() -> Response<Body> {
-    Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(Body::empty())
-        .unwrap()
-}
-
-pub fn method_not_allowed() -> Response<Body> {
-    Response::builder()
-        .status(StatusCode::METHOD_NOT_ALLOWED)
-        .body(Body::empty())
-        .unwrap()
-}
-
-pub fn internal_server_error() -> Response<Body> {
-    Response::builder()
-        .status(StatusCode::INTERNAL_SERVER_ERROR)
-        .body(Body::from("an unexpected error occurred"))
-        .unwrap()
-}
-
-pub fn bad_request(user_error: &WebErrorMessage) -> Response<Body> {
-    Response::builder()
-        .status(StatusCode::BAD_REQUEST)
-        .body(Body::from(user_error.message()))
-        .unwrap()
-}
-
-pub fn accepted() -> Response<Body> {
-    Response::builder()
-        .status(StatusCode::ACCEPTED)
-        .body(Body::empty())
-        .unwrap()
-}
-
-//TODO: Finish
 pub fn status(status_code: StatusCode) -> Response<Body> {
     Response::builder()
         .status(status_code)
@@ -43,18 +7,9 @@ pub fn status(status_code: StatusCode) -> Response<Body> {
         .unwrap()
 }
 
-pub enum WebErrorMessage {
-    ZeroUserId,
-}
-
-impl WebErrorMessage {
-    fn message(&self) -> String {
-        let mut error_string = String::from("error: ");
-        match self {
-            WebErrorMessage::ZeroUserId => {
-                error_string.push_str("user id cannot be 0");
-            }
-        }
-        error_string
-    }
+pub fn status_with_message(status_code: StatusCode, message: &str) -> Response<Body> {
+    Response::builder()
+        .status(status_code)
+        .body(Body::from(String::from(message)))
+        .unwrap()
 }
